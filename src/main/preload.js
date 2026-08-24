27d034b1c5a856670a5e749e4377de3b2c5f51f8
checkner89 <96750156+checkner89@users.noreply.github.com>
2026-08-24 14:46:05 +0200

    Expose status and update controls to renderer

--- src/main/preload.js
+++ src/main/preload.js
@@ -2,22 +2,21 @@
 
 const { contextBridge, ipcRenderer } = require('electron');
 
+function subscribe(channel, callback) {
+  const listener = (_event, payload) => callback(payload);
+  ipcRenderer.on(channel, listener);
+  return () => ipcRenderer.removeListener(channel, listener);
+}
+
 contextBridge.exposeInMainWorld('flightDeck', {
+  getVersion: () => ipcRenderer.invoke('app:get-version'),
+
+  getSimConnectStatus: () => ipcRenderer.invoke('simconnect:get-status'),
   retrySimConnect: () => ipcRenderer.invoke('simconnect:retry'),
+  onSimConnectStatus: (callback) => subscribe('simconnect:status', callback),
+
+  getUpdateStatus: () => ipcRenderer.invoke('update:get-status'),
   checkForUpdates: () => ipcRenderer.invoke('update:check'),
-  onSimConnectStatus: (callback) => {
-    const listener = (_event, payload) => callback(payload);
-    ipcRenderer.on('simconnect:status', listener);
-    return () => ipcRenderer.removeListener('simconnect:status', listener);
-  },
-  onUpdateStatus: (callback) => {
-    const listener = (_event, payload) => callback(payload);
-    ipcRenderer.on('update:status', listener);
-    return () => ipcRenderer.removeListener('update:status', listener);
-  },
-  onVersion: (callback) => {
-    const listener = (_event, version) => callback(version);
-    ipcRenderer.on('app:version', listener);
-    return () => ipcRenderer.removeListener('app:version', listener);
-  }
+  installUpdate: () => ipcRenderer.invoke('update:install'),
+  onUpdateStatus: (callback) => subscribe('update:status', callback)
 });
